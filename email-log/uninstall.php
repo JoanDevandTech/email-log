2011839f1df3b3eba83650605a1c6aab5a1acfcf
Joan Dev & Tech <joan@joandev.com>
2026-01-12 22:16:16 +0100

    Adjustment commit | Free Version

--- email-log/uninstall.php
+++ email-log/uninstall.php
@@ -6,17 +6,15 @@
  */
 
 // exit if WordPress is not uninstalling the plugin.
-if ( ! defined( 'ABSPATH' ) && ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+if (!defined('ABSPATH') && !defined('WP_UNINSTALL_PLUGIN')) {
 	exit();
 }
 
-if ( is_multisite() ) {
-	// Note: if there are more than 10,000 blogs or
-	// if `wp_is_large_network` filter is set, then this may fail.
+if (is_multisite()) {
 	$sites = get_sites();
 
-	foreach ( $sites as $site ) {
-		switch_to_blog( $site->blog_id );
+	foreach ($sites as $site) {
+		switch_to_blog($site->blog_id);
 		email_log_delete_db_data();
 		restore_current_blog();
 	}
@@ -33,41 +31,42 @@ if ( is_multisite() ) {
  *
  * @global object $wpdb
  */
-function email_log_delete_db_data() {
+function email_log_delete_db_data()
+{
 	global $wpdb;
 
 	$remove_data_on_uninstall = false;
 
-	$option = get_option( 'email-log-core' );
-	if ( is_array( $option ) && array_key_exists( 'remove_on_uninstall', $option ) &&
-	     'true' === strtolower( $option['remove_on_uninstall'] ) ) {
+	$option = get_option('email-log-core');
+	if (
+		is_array($option) && array_key_exists('remove_on_uninstall', $option) &&
+		'true' === strtolower($option['remove_on_uninstall'])
+	) {
 
 		$remove_data_on_uninstall = true;
 	}
 
-	// This is hardcoded on purpose, since the entire plugin is not loaded during uninstall.
 	$table_name = $wpdb->prefix . 'email_log';
 
-	if ( $remove_data_on_uninstall ) {
-		if ( $wpdb->get_var( "SHOW TABLES LIKE '{$table_name}'" ) == $table_name ) { //phpcs:ignore
-			$wpdb->query( "DROP TABLE $table_name" ); //phpcs:ignore
+	if ($remove_data_on_uninstall) {
+		if ($wpdb->get_var("SHOW TABLES LIKE '{$table_name}'") == $table_name) { //phpcs:ignore
+			$wpdb->query("DROP TABLE $table_name"); //phpcs:ignore
 		}
 
-		delete_option( 'email-log-db' );
-		delete_option( 'email-log-core' );
+		delete_option('email-log-db');
+		delete_option('email-log-core');
 
 		$roles = get_editable_roles();
-		foreach ( $roles as $role_name => $role_obj ) {
-			$role = get_role( $role_name );
+		foreach ($roles as $role_name => $role_obj) {
+			$role = get_role($role_name);
 
-			if ( ! is_null( $role ) ) {
-				$role->remove_cap( 'manage_email_logs' );
+			if (!is_null($role)) {
+				$role->remove_cap('manage_email_logs');
 			}
 		}
-		// Mask Fields addon adds this option.
-		delete_option( 'el_mask_fields' );
+		delete_option('el_mask_fields');
 
-		delete_option( 'el_bundle_license' );
-		$wpdb->query( "DELETE FROM {$wpdb->options} WHERE option_name LIKE 'el_license_%'" ); //phpcs:ignore
+		delete_option('el_bundle_license');
+		$wpdb->query("DELETE FROM {$wpdb->options} WHERE option_name LIKE 'el_license_%'"); //phpcs:ignore
 	}
 }
